7da713eb07b4ca50aa50a264a12de1a6b229ddf4
shubham5104 <shubhamkundalwal84@gmail.com>
2025-04-21 21:33:07 +0530

    sortAscending

--- Programs/src/arrayPractice/SortingArray.java
+++ Programs/src/arrayPractice/SortingArray.java
@@ -5,7 +5,7 @@ public class SortingArray {
 	public static void main(String[] args) {
 		int[] arr= {4,3,6,22};
 		
-		sortDecending(arr);
+		sortAscending(arr);
 	}
 	
 	static void sortDecending(int[] arr) {
@@ -26,7 +26,25 @@ public class SortingArray {
 		{
 			System.out.println(arr[i]);
 		}
-		
+	}
+	static void sortAscending(int[] arr) 
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]>arr[j])
+				{
+					int temp=arr[j];
+					arr[j]=arr[i];
+					arr[i]=temp;
+				}
+			}
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
 	}
 
 }
