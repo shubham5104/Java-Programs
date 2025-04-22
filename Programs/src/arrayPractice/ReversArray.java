f51a8c1f343a721b474720d8d116f125c3b8c56b
shubham5104 <shubhamkundalwal84@gmail.com>
2025-04-22 23:30:54 +0530

    Wap print it in reverse order.

---
+++ Programs/src/arrayPractice/ReversArray.java
@@ -0,0 +1,17 @@
+package arrayPractice;
+
+// Wap print it in reverse order.
+public class ReversArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr= {2,4,5,3,11,7,66,55,44};
+		for(int i=arr.length-1;i>=0;i--)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+
+}
