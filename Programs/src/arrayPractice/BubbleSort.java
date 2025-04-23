d885aadfb96581644159df69bd4ceff8c903a556
shubham5104 <shubhamkundalwal84@gmail.com>
2025-04-23 22:34:10 +0530

    ascending Sort and Squre

--- Programs/src/arrayPractice/BubbleSort.java
+++ Programs/src/arrayPractice/BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort {
 		ascendingSort(arr);
 		displayArray(arr);
 		System.out.println();
-		descendingSort(arr);
+		ascendingSortandSqure(arr);
 		displayArray(arr);
 		
 	}
@@ -44,6 +44,25 @@ public class BubbleSort {
 				
 			}
 		}
+		return arr;
+	}
+	static int[] ascendingSortandSqure(int[] arr) {
+		for(int i=0;i<arr.length-1;i++)
+		{
+			for(int j=0;j<arr.length-i-1;j++)
+			{
+				if(arr[j]>arr[j+1]) 
+				{
+					int temp = arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+				
+			}
+		}
+		for(int i = 0; i < arr.length; i++) {
+	        arr[i] = arr[i] * arr[i];
+	    }
 		return arr;
 	}
 	static void displayArray(int[]arr) {
