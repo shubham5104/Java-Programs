561c00b447672ef5972471c281ae1b9f6e9e08d1
shubham5104 <shubhamkundalwal84@gmail.com>
2025-04-23 22:12:45 +0530

    descending Bubble Sort

--- Programs/src/arrayPractice/BubbleSort.java
+++ Programs/src/arrayPractice/BubbleSort.java
@@ -6,6 +6,15 @@ public class BubbleSort {
 		// TODO Auto-generated method stub
 		int[] arr= {1,3,6,4,9,6};
 
+		ascendingSort(arr);
+		displayArray(arr);
+		System.out.println();
+		descendingSort(arr);
+		displayArray(arr);
+		
+	}
+	
+	static int[] ascendingSort(int[] arr) {
 		for(int i=0;i<arr.length-1;i++)
 		{
 			for(int j=0;j<arr.length-i-1;j++)
@@ -19,7 +28,23 @@ public class BubbleSort {
 				
 			}
 		}
-		displayArray(arr);
+		return arr;
+	}
+	static int[] descendingSort(int[] arr) {
+		for(int i=0;i<arr.length-1;i++)
+		{
+			for(int j=0;j<arr.length-i-1;j++)
+			{
+				if(arr[j]<arr[j+1]) 
+				{
+					int temp = arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+				
+			}
+		}
+		return arr;
 	}
 	static void displayArray(int[]arr) {
 		for(int n:arr)
