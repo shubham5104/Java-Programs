2155521eb602c80de82cd16fe01e92735ae12d63
shubham5104 <shubhamkundalwal84@gmail.com>
2025-04-23 21:53:51 +0530

    Bubble Sort

---
+++ Programs/src/arrayPractice/BubbleSort.java
@@ -0,0 +1,31 @@
+package arrayPractice;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {1,3,6,4,9,6};
+
+		for(int i=0;i<arr.length-1;i++)
+		{
+			for(int j=0;j<arr.length-i-1;j++)
+			{
+				if(arr[j]>arr[j+1]) 
+				{
+					int temp = arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+				
+			}
+		}
+		displayArray(arr);
+	}
+	static void displayArray(int[]arr) {
+		for(int n:arr)
+		{
+			System.out.print(n+" ");
+		}
+	}
+
+}
