fdad0629ad49e5c0bc7e96236209fc46849a4454
shubham5104 <shubhamkundalwal84@gmail.com>
2025-04-24 23:56:38 +0530

    count given String charecter

--- Programs/src/stringspprogram/ReverseString.java
+++ Programs/src/stringspprogram/ReverseString.java
@@ -5,7 +5,10 @@ public class ReverseString {
 	public static void main(String[] args) {
 		
 		String str="Shubham";
+		String find="S";
 		reverse(str);
+		System.out.println();
+		System.out.println(countcharecter(str, find));
 
 	}
 
@@ -14,5 +17,20 @@ public class ReverseString {
 		{
 			System.out.print(str.charAt(i));
 		}
+	}
+	static int countcharecter(String str,String f)
+	{
+		int count=0;
+		int index=str.indexOf(f);
+		for(int i=0;i<str.length();i++)
+		{
+			while(index!=-1)
+			{
+				count++;
+				index =str.indexOf(f,index+1);
+			}
+		}
+		
+		return count;
 	}
 }
