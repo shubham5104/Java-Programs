15732bd9c8df9901961a7312be58ea61a3050a57
shubham5104 <shubhamkundalwal84@gmail.com>
2025-05-15 09:41:25 +0530

    Fibonacci Series

---
+++ Programs/src/com/interview/prep/Fibonacci.java
@@ -0,0 +1,21 @@
+package com.interview.prep;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n=15;
+		int n1=0;
+		int n2=1;
+		System.out.print(n1+" "+n2+" ");
+		for(int i=1;i<=n-2;i++)
+		{
+			int sum=n1+n2;
+			n1=n2;
+			n2=sum;
+			System.out.print(sum+" ");
+		}
+	}
+
+}
