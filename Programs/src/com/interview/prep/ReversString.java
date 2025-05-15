3da7669e16dc34638ce74fe34bebba08d3cc0de6
shubham5104 <shubhamkundalwal84@gmail.com>
2025-05-15 09:15:15 +0530

    Revers String with StringBuilder

--- Programs/src/com/interview/prep/ReversString.java
+++ Programs/src/com/interview/prep/ReversString.java
@@ -7,12 +7,15 @@ public class ReversString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str="Hello";
+		StringBuilder rev=new StringBuilder();
 		
 		for(int i=str.length()-1;i>=0;i--)
 		{
 			char ch=str.charAt(i);
-			System.out.print(ch);
+			rev.append(ch);
 		}
+		System.out.println(str);
+		System.out.println(rev);
 
 	}
 
