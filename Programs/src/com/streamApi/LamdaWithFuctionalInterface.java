163ec9a5edd42856076f41406406617b7ea7b599
shubham5104 <shubhamkundalwal84@gmail.com>
2025-05-31 12:04:00 +0530

    functional interface

--- Programs/src/com/streamApi/LamdaWithFuctionalInterface.java
+++ Programs/src/com/streamApi/LamdaWithFuctionalInterface.java
@@ -7,6 +7,7 @@ public class LamdaWithFuctionalInterface {
 	public static void main(String[] args) {
 		
 		Function<Integer, Integer> square= n->n*n;
+		
 		System.out.println(square.apply(5));
 
 	}
